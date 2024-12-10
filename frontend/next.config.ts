77a0a758d490e52c9951ec82298eb18b426c858b
Fernanda Bezerra <fbn.prg@gmail.com>
2024-12-10 15:32:49 -0400

    Deleting old frontend and creating a new one with Next.js

---
+++ frontend/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
