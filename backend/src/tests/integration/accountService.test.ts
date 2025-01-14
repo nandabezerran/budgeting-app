24cb8827f36882515683c940f3f7fac2130c56e7
Fernanda Bezerra <fbn.prg@gmail.com>
2025-01-14 16:59:48 -0400

    fix: no need for fake timers

--- backend/src/tests/integration/accountService.test.ts
+++ backend/src/tests/integration/accountService.test.ts
@@ -2,8 +2,9 @@ import request from 'supertest';
 import app from '../../../index'; // Import the Express app
 import { Account } from '../../../src/entities/Account'; // Import your Account entity
 import { AccountType } from '../../enums/AccountType';
+import AppDataSource from '../../../typeorm.config'; // Import your TypeORM DataSource
 
-jest.useFakeTimers();
+
 jest.mock('../../entities/Account');
 
 describe('POST /api/acount', () => {
